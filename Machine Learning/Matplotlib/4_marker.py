37abe425747a754046e91aee1206e7c1fe49dad8
Mallesh <malleshmb98@gmail.com>
2024-04-19 12:46:25 +0530

    added matplotlib and seaborn

---
+++ Machine Learning/Matplotlib/4_marker.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt #for visualization
+import  numpy as np #for array operations
+x = np.array([1,2,6,8])
+y = np.array([3,8,7,9])
+plt.plot(x,y,linewidth=1,color='black',marker='o')
+plt.show()
